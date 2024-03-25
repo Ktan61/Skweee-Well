4ed07a8fb54c306f81427bf6624e9f9bf0233c0f
madelinestruthers <mstruthers1@my.bcit.ca>
2024-03-25 09:13:12 -0700

    adding footer component and styling

---
+++ components/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+export default function Footer(){
+
+    return(
+        <>
+        <footer className={styles.footer}>
+        ©<br></br> Madeline Struthers, Pia Mendoza, Kathryn Tan
+        </footer>
+        </>
+    )
+}
