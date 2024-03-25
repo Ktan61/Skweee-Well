fa2436dabbedd2416fc88d24402074a4dfb347b6
KathrynTan <ktan61@my.bcit.ca>
2024-03-24 23:26:35 -0700

    K: Created components files and folders for footer, header, navbar.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
@@ -10,6 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar/>
       <main className={`${styles.main}`}>
         Eskweeet-it!
       </main>
