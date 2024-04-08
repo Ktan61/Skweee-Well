40ab2f57df7e0aa504fc281ad59ff60c0874f21d
KathrynTan <ktan61@my.bcit.ca>
2024-04-08 04:16:21 -0700

    K/ Made updates to global css and added fonts to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import { KoHo, Nunito } from "next/font/google";
+
+const fontKoHo = KoHo({
+  weight: [ '200', '300', '400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
+
+const fontNunito = Nunito({
+  style: ['normal'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${fontKoHo.style.fontFamily};
+        }
+
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
