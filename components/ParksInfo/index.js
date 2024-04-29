e10300ef5ff4f03bc4417fee3cd3e0a178156f4c
Pia Mendoza <lmendoza22@my.bcit.ca>
2024-04-29 16:44:31 -0700

    Park cards are now links

--- components/ParksInfo/index.js
+++ components/ParksInfo/index.js
@@ -1,18 +1,39 @@
+import Link from 'next/link';
 import styles from './ParksInfo.module.css';
-import { inventory } from "@/data/parksData";
-
 
 export default function ParksInfo({ parksData }) {
     const { image, parkName } = parksData;
+    const parkLinks = {
+        "Robert Burnaby Park": "https://www.burnaby.ca/explore-outdoors/parks/robert-burnaby-park",
+        "Burnaby Mountain Conservation Area": "https://www.burnaby.ca/explore-outdoors/parks/burnaby-mountain-conservation-area",
+        "Central Park": "/CentralPark",
+        "Taylor Park": "https://www.burnaby.ca/explore-outdoors/parks/taylor-park",
+        "Confederation Park": "https://www.burnaby.ca/explore-outdoors/parks/confederation-park",
+        "Kensington Park": "https://www.burnaby.ca/explore-outdoors/parks/kensington-park"
+    };
+
+
+    const isExternalLink = parkLinks[parkName].startsWith("http");
 
     return (
         <div className={styles.PC_Container}>
-             <div className={`${styles.parkCard}`}>
-                <p className={styles.PC_ImageDiv}>{image}</p>
-                <div className={styles.PC_TextDiv}>
-                    <h5 className={styles.PC_TextHeader}>{parkName}</h5>
-                </div>
-             </div>
+            {isExternalLink ? (
+                <Link href={parkLinks[parkName]} target="_blank" rel="noopener noreferrer" className={styles.parkCard}>
+                    <p className={styles.PC_ImageDiv}>{image}</p>
+                    <div className={styles.PC_TextDiv}>
+                        <h5 className={styles.PC_TextHeader}>{parkName}</h5>
+                    </div>
+                </Link>
+            ) : (
+                <Link href={parkLinks[parkName]} passHref>
+                    <div className={styles.parkCard}>
+                        <p className={styles.PC_ImageDiv}>{image}</p>
+                        <div className={styles.PC_TextDiv}>
+                            <h5 className={styles.PC_TextHeader}>{parkName}</h5>
+                        </div>
+                    </div>
+                </Link>
+            )}
         </div>
     );
 }
